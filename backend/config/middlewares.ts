ed6143027b301c8d4da88d1f992da4725a1d6f7c
iquesambi <30755769+iquesambi@users.noreply.github.com>
2026-05-05 14:52:05 -0300

    CORS: ensure Firebase is configured

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -3,15 +3,30 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: false,
+    },
+  },
+
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'https://tv1-site.web.app',
+        'https://slacking-stylized-scheme.ngrok-free.dev',
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
+      headers: '*',
+      credentials: true,
     },
   },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
